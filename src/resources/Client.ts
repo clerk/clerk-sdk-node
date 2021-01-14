b9d5efdd7eba599a9560e0c1312bd80a4965c50c
Mark Pitsilos <mark.pitsilos@gmail.com>
2021-01-14 22:15:57 +0200

    Align JS types with server Go types

--- src/resources/Client.ts
+++ src/resources/Client.ts
@@ -2,27 +2,23 @@ import type {
   ClientJSON,
   ClientResource,
 } from "../types/resources";
-import { Session } from "./Session";
 
 export class Client implements ClientResource {
   id: string;
-  ended: boolean;
-  sessions: Session[];
+  sessionIds: string[];
   signInAttemptId: string | null;
   signUpAttemptId: string | null;
   lastActiveSessionId: string | null;
+  createdAt: number;
+  updatedAt: number;
 
   constructor(data: ClientJSON) {
     this.id = data.id;
-    this.ended = data.ended;
-    this.sessions =
-      data.sessions
-        ? data.sessions.map(
-            (x) => new Session(x)
-          )
-        : [];
+    this.sessionIds = data.session_ids;
     this.signUpAttemptId = data.sign_in_attempt_id;
     this.signInAttemptId = data.sign_in_attempt_id;
     this.lastActiveSessionId = data ? data.last_active_session_id : null;
+    this.createdAt = data.created_at;
+    this.updatedAt = data.updated_at;
   }
 }
