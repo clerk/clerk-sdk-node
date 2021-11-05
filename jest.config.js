1566924db50ba5e089251e8568cdf26855be4e41
Peter Perlepes <p.perlepes@gmail.com>
2021-11-05 13:55:22 +0200

    feat: Setup jest setupFiles and environments initialization

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,8 @@
 module.exports = {
-  "roots": [
-    "<rootDir>/src"
-  ],
-  "testMatch": [
-    "**/?(*.)+(spec|test).+(ts|tsx|js)"
-  ],
-  "transform": {
-    "^.+\\.(ts|tsx)$": "ts-jest"
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/?(*.)+(spec|test).+(ts|tsx|js)'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
   },
-}
+  setupFiles: ['<rootDir>/jest/setupEnvVars.js'],
+};
