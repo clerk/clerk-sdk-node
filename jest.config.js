cc03fda25132ce7fe2bef5fa056d07557788b586
Peter Perlepes <p.perlepes@gmail.com>
2021-11-30 09:49:10 +0200

    feat: Update building/bundling structures

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,8 @@ module.exports = {
   roots: ['<rootDir>/src'],
   testMatch: ['**/?(*.)+(spec|test).+(ts|tsx|js)'],
   transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest',
+    '^.+\\.ts$': 'ts-jest',
   },
   setupFiles: ['<rootDir>/jest/setupEnvVars.js'],
+  collectCoverage: true,
 };
