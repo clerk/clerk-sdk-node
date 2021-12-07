60a6591b45a3de58c1f4607091bbfeab93fbf446
Peter Perlepes <p.perlepes@gmail.com>
2021-12-07 11:42:19 +0200

    feat: Add tag message on deploy script

--- scripts/publish.js
+++ scripts/publish.js
@@ -21,7 +21,7 @@ async function publish(publishType) {
     /* 3.  Git actions */
     await exec('git add -A');
     await exec(`git commit -am "build: ${require('../package.json').version}"`);
-    await exec(`git tag -a v${require('../package.json').version}`);
+    await exec(`git tag -a v${require('../package.json').version} -m 'v${require('../package.json').version}'`);
     await exec('git push && git push --tags');
 
     /* 4. Publish to npm */
